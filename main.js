bbf9aebb06840121ad702759c9ef378d0e42491f
AndresBLV <77251658+AndresBLV@users.noreply.github.com>
2023-02-05 15:23:28 -0400

    paginas

--- main.js
+++ main.js
@@ -24,3 +24,12 @@ point.forEach( (eachPoint,i)=> {
 
     })
 })
+
+function accion(){
+    var ancla = document.getElementsByClassName('nav-enlace')
+
+    for(var i = 0; i < ancla.length; i++){
+        ancla[i].classList.toggle('desaparece')
+    }
+}
+ 
